8df8f8bbb0ffd5715c5668836234fb805c72b104
bugtree <sbugbtree@gmail.com>
2017-08-29 10:49:35 +0800

    i1.Ôö¼ÓcssÑùʽäÖȾ2.ʵÏÖһ¸ödemo,chkbkb²鿴csskbkbkbÔö¼ÓcssºóµÄЧ¹û£º£ºÎңº£º:wq:w:q:q!

--- webapp/app.py
+++ webapp/app.py
@@ -78,11 +78,25 @@ async def response_factory(app, handler):
         return resp
     return response
 
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
 async def init(loop):
     await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='bugtree', password='bugtree', db='blog')
     app = web.Application(loop=loop, middlewares=[
         logger_factory, response_factory])
-    init_jinja2(app)
+    init_jinja2(app, filters=dict(datetime=datetime_filter))
+    add_static(app)
     add_routes(app, 'handlers')
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info("server started at http://127.0.0.1:9000...")
